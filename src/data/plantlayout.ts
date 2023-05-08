1c9ac80b9cd151afe740549189d21294d7a7a2cc
lihuaPan <lihua.pan@hec.ca>
2023-05-08 12:09:15 -0400

    remove Bradenton page

--- src/data/plantlayout.ts
+++ src/data/plantlayout.ts
@@ -5,7 +5,7 @@ export interface PlantLayout {
 export const plantlayouts: Record<string, PlantLayout> = {
   'CA1': {ready: false},
   CA2: {ready: false},
-  Bradenton: {ready: true},
+  Bradenton: {ready: false},
   FL2: {ready: false},
   DC: {ready: false},
   NJ: {ready: false},
